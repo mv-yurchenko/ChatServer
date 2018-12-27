13a63d762955770246d7d95d7a397aa407a70b18
Unknown <maxonus2@gmail.com>
2018-12-27 11:00:46 +0300

    Fixed some issues in __connect__ function

--- Client/Client.py
+++ Client/Client.py
@@ -3,14 +3,22 @@ import threading
 import time
 
 HOST = "192.168.56.1"
+
+
 class Client:
 
-    def __init__(self):
+    def __init__(self, username):
+        self.username = username
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.__connect__()
+        self.__calculate_key__()
+        print(self.key)
 
     def send_message(self):
-        self.sock.send(bytes(input(), 'utf-8'))
+        self.sock.sendto(bytes(input(), 'utf-8'), self.server)
+
+    def receiving(self):
+        pass
 
     def __connect__(self):
         # host = socket.gethostbyname(socket.gethostname())
@@ -20,6 +28,16 @@ class Client:
         self.sock.bind((host, port))
         self.sock.setblocking(False)
         self.sock.sendto(b"user connected", self.server)
-        self.sock.close()
 
-Client()
+    def __calculate_key__(self): 
+        sum = 0 
+        mult = 1
+        for char in self.username: 
+            sum += ord(char)
+            mult *= ord(char)
+        self.key = int((sum + mult) / 2)
+
+
+a = Client('user')
+while 1:
+    a.send_message()
