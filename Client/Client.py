9d608fac6ffa68ddcbc3cb023023cb7bf17dd1be
Unknown <maxonus2@gmail.com>
2018-12-31 21:33:11 +0300

    Ready for 1.1 Release

--- Client/Client.py
+++ Client/Client.py
@@ -106,6 +106,10 @@ class Client:
 
 login = input("Input login: ")
 a = Client(login)
-msg = input()
-a.send_message(msg)
+fl = True
+while fl:
+    msg = input()
+    if msg == "exit":
+        fl = False
+    a.send_message(msg)
 a.close_client()
