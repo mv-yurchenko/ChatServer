751fb32f261afe108c6ebb6c5c6d7026de78e28c
Max Yurchenko <maxonus2@gmail.com>
2019-01-07 23:36:34 +0300

    Added Tkinter GUI (IT WORKS!)

--- tkinter_gui.py
+++ tkinter_gui.py
@@ -1,20 +1,38 @@
 from tkinter import *
 from threading import Thread
+from Client.Client import Client
 
-tk=Tk()
 
-text=StringVar()
-name=StringVar()
-name.set('HabrUser')
-text.set('')
-tk.title('MegaChat')
-tk.geometry('400x300')
+class ClientGUI(Client):
 
-log = Text(tk)
-nick = Entry(tk, textvariable=name)
-msg = Entry(tk, textvariable=text)
-msg.pack(side='bottom', fill='x', expand='true')
-nick.pack(side='bottom', fill='x', expand='true')
-log.pack(side='top', fill='both',expand='true')
+    def __init__(self):
 
-tk.mainloop()
+        self.window = Tk()
+        self.window.title("ChatClient")
+        self.window.geometry("800x600")
+        send_message_line_height = 25
+        text_windows_width = 560
+        second_column_start = 580
+        second_column_width = 200
+
+        self.message_input = Text(self.window).place(x=10, y=570, width=text_windows_width,
+                                                     height=send_message_line_height)
+
+        self.send_message_button = Button(self.window, text="Send message")
+        self.send_message_button.place(x=second_column_start, y=570,
+                                       width=second_column_width, height=send_message_line_height)
+
+        self.messages_output = Text(self.window).place(x=10, y=10,
+                                                       width=text_windows_width, height=550)
+
+        self.login_input = Text(self.window).place(x=second_column_start, y=10,
+                                                   width=second_column_width, height=send_message_line_height)
+
+        self.accept_login_button = Button(self.window, text="Accept Login")
+        self.accept_login_button.place(x=second_column_start, y=50,
+                                       width=second_column_width, height=send_message_line_height)
+
+        self.window.mainloop()
+
+
+a = ClientGUI()
