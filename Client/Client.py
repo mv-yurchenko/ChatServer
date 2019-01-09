f166d0a1a3eca6722e43a9ffd36c2d1c59d42c90
Max Yurchenko <maxonus2@gmail.com>
2019-01-09 21:53:07 +0300

    Merged tkinterGUI in Client.py

--- Client/Client.py
+++ Client/Client.py
@@ -1,6 +1,8 @@
 import socket
-import tkinter as tk
+from threading import Thread
 import threading
+from tkinter import messagebox
+from tkinter import *
 import time
 from Cryptography.Cryptography import Cryptography
 from queue import Queue
@@ -38,6 +40,8 @@ class Client:
         if self.is_private_talk:
             self.msg_receiver = "private"
             self.companion_login = companion_login
+        self.gui_initialize()
+
 
     def close_client(self):
         """Закрывает сокет и останавливает поток"""
@@ -68,7 +72,7 @@ class Client:
                     decrypted_msg = self.__decrypt_msg__(msg_text.encode("utf-8"))
                     time.sleep(0.2)
                     print(decrypted_msg)
-                    self.tex.insert(tk.END, decrypted_msg)
+                    self.messages_output.insert(END, str(self.messages_history))
                     # Add time for output
                     self.messages_history.append((msg_time, sender, decrypted_msg))
             except Exception as _:
@@ -104,3 +108,54 @@ class Client:
     @staticmethod
     def __get_current_time__() -> str:
         return time.strftime("%Y-%m-%d-%H.%M.%S", time.localtime())
+
+    def gui_initialize(self):
+        self.window = Tk()
+        self.window.title("ChatClient")
+        self.window.geometry("800x600")
+        send_message_line_height = 25
+        text_windows_width = 560
+        second_column_start = 580
+        second_column_width = 200
+
+        self.message_input = Text(self.window)
+        self.message_input.place(x=10, y=570, width=text_windows_width,
+                                 height=send_message_line_height)
+
+        self.send_message_button = Button(self.window, text="Send message", command=self.send_message_button_clicked)
+        self.send_message_button.place(x=second_column_start, y=570,
+                                       width=second_column_width, height=send_message_line_height)
+
+        self.messages_output = Text(self.window)
+        self.messages_output.place(x=10, y=10,
+                                   width=text_windows_width, height=550)
+
+        self.login_input = Text(self.window)
+        self.login_input.place(x=second_column_start, y=10,
+                               width=second_column_width, height=send_message_line_height)
+
+        self.accept_login_button = Button(self.window, text="Accept Login", relief=GROOVE,
+                                          command=self.accept_login_button_clicked)
+        self.accept_login_button.place(x=second_column_start, y=50,
+                                       width=second_column_width, height=send_message_line_height)
+
+        self.window.mainloop()
+
+    def accept_login_button_clicked(self):
+
+        user_login = self.login_input.get("1.0", END)
+
+        if not self.__is_login_input_empty__():
+            print(user_login)
+            self.messages_output.insert(END, "Welcome to Chat")
+        else:
+            messagebox.showerror("No login", "Input Login!")
+
+    def __is_login_input_empty__(self) -> bool:
+        return self.login_input.compare("end-1c", "==", "1.0")
+
+    def send_message_button_clicked(self):
+        self.send_message(self.message_input.get("1.0", END))
+
+
+a = Client("user")
